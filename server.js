b033801fb80495c803ab43e59bfc255b4209249a
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-07-09 00:07:17 +0100

    setting post route

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const welcomeMessage = {
   id: 0,
   from: "Bart",
   text: "Welcome to CYF chat system!",
+
 };
 
 //This array is our "data store".
@@ -20,4 +23,27 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages",(req, res)=>{
+  res.send(messages)
+})
+app.post("/messages/add", (req, res)=>{
+  const {from, text} = req.body;
+  if(from && text){
+    const message = {
+      from: from,
+      text: text
+            }
+          messages.push(message)
+    res.send({"success" : true});
+
+  }else{
+    res.sendStatus(400);
+  }
+  
+
+
+})
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
